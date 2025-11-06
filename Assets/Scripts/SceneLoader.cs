6642c78ac4593e738c02a6d76c551a29ee1fa251
Luke Sarun Tighe <DOGEisLIFE2018@gmail.com>
2025-11-05 21:41:44 -0600

    more menu stuff

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance { get; private set; }
+    private void Awake()
+    {
+        // Singleton pattern setup
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void SceneSwitch(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
